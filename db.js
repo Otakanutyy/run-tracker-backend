6a81041dccb3f4f48b92a45ad4209f90b8aeb73b
Otakanutyy <kogsaitama@gmail.com>
2025-07-22 22:16:24 +0500

    try999

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  host: process.env.PGHOST,      
+  port: process.env.PGPORT,       
+  user: process.env.PGUSER,        
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE
 });
 
 module.exports = pool;
